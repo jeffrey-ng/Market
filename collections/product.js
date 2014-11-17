3d90086e034ac1ee9fbf23e57bfe3fff90ed88e9
Nicolas Angelard-Gontier <gontier.nicolas@yahoo.fr>
2014-11-17 17:07:20 -0500

    propoer indentation for product collection.

--- collections/product.js
+++ collections/product.js
@@ -5,43 +5,50 @@ Schemas={}
 
 Schemas.Product = new SimpleSchema({
     _id: {
-        type: String,
-        regEx: SimpleSchema.RegEx.Id
+      type: String,
+      regEx: SimpleSchema.RegEx.Id
     },
+
     name: {
-        type: String,
-        optional:false
+      type: String,
+      optional:false
     },
-   price: {
-    type: Number,
-    min: 0,
-    decimal: true,
-    label: 'Price'
-   },
 
-   quantity: {
-    type: Number,
-    min: 0,
-    label: 'Quantity'
-   },
+    description: {
+      type: String,
+      optional:false
+    },
 
-   createdBy: {
-        type: String,
-        optional: false
-   },
+    price: {
+      type: Number,
+      min: 0,
+      decimal: true,
+      label: 'Price'
+    },
 
-   createdAt: {
-    type: Date,
-      autoValue: function() {
-        if (this.isInsert) {
-          return new Date;
-        } else if (this.isUpsert) {
-          return {$setOnInsert: new Date};
-        } else {
-          this.unset();
+    quantity: {
+      type: Number,
+      min: 0,
+      label: 'Quantity'
+    },
+
+    createdBy: {
+      type: String,
+      optional: false
+    },
+
+    createdAt: {
+      type: Date,
+        autoValue: function() {
+          if (this.isInsert) {
+            return new Date;
+          } else if (this.isUpsert) {
+            return {$setOnInsert: new Date};
+          } else {
+            this.unset();
+          }
         }
-      }
-  }
+    }
 });
 
 Products.attachSchema(Schemas.Product);
