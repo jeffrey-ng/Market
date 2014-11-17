84b061a18963ef1cea949a84a857489326336bbc
Nicolas Angelard-Gontier <gontier.nicolas@yahoo.fr>
2014-11-17 17:54:24 -0500

    updated random products creation

--- server/seeds.js
+++ server/seeds.js
@@ -6,17 +6,18 @@ Meteor.startup(function() {
   });
 
   Factory.define('product', Products, {
-    name: function() { return Fake.sentence();},
+    name: function() { return Fake.sentence(); },
     price: function() { return _.random(1, 5); },
-    quantity: function() { return _.random(1, 5); }
-
+    quantity: function() { return _.random(1, 5); },
+    description: function() { return Fake.sentence(); },
+    createdBy: function() { return Fake.sentence(); }
   })
 
-if (Products.find({}).count() === 0) {
-_(10).times(function(n) {
+  if (Products.find({}).count() === 0) {
+    _(10).times(function(n) {
       Factory.create('product');
     });
-}
+  }
 
 
   if (Items.find({}).count() === 0) {
