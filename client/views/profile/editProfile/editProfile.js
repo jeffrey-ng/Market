1f9bebc1499d995400e5c7a5b41fd28fe798eae4
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2014-11-17 22:29:28 -0500

    Edit Profile

--- client/views/profile/editProfile/editProfile.js
+++ client/views/profile/editProfile/editProfile.js
@@ -2,9 +2,11 @@ Template.editProfile.events({
     'submit form#editProfileForm': function (e, t) {
         e.preventDefault();
         var fullName = t.find('#edit-fullName').value;
-        Meteor.call('editProfile', Meteor.userId(), fullName, function (error, result) {
+        var website = t.find('#edit-website').value;
+        var bio = t.find('#edit-bio').value;
+        Meteor.call('editProfile', Meteor.userId(), fullName,website,bio, function (error, result) {
             if(error) console.log(error);
-            Router.go('profile');
+            Router.go('profile', {_id: Meteor.userId()});
         });
     }
 });
--- server/methods.js
+++ server/methods.js
@@ -1,6 +1,6 @@
 Meteor.methods({
-    editProfile: function(userId, fullName) {
-        Meteor.users.update({_id: userId}, {$set: {'profile.fullName': fullName}});
+    editProfile: function(userId, fullName, website, bio) {
+        Meteor.users.update({_id: userId}, {$set: {'profile.fullName': fullName, 'profile.website': website, 'profile.bio':bio}});
 
     },
     createProduct: function(userId, productName, price, number) {
