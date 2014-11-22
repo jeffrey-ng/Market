90e669dfb44fdd84f5e2aede47e65f7a68d3b296
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2014-11-22 14:47:00 -0500

    Search for dashboard.

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -8,22 +8,3 @@ Template.dashboard.helpers({
 
     }
 });
-
-Template.dashboard.events = {
-    'keydown input#itemInput' : function (event) {
-        if (event.which == 13) {//enter key
-            var name = document.getElementById('itemInput');
-            var rating = 3;
-
-            if (name.value != '') {
-                Items.insert({
-                   name: name.value,
-                   rating: rating
-                });
-
-                document.getElementById('itemInput').value ='';
-                item.value = '';
-            }
-        }
-    }
-}
